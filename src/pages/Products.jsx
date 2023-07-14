8fe948727af9d9339555ae52884031d130ad1031
Bkind254 <langatbrian02@gmail.com>
2023-07-14 11:36:57 +0300

    5th commit

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -11,23 +11,23 @@ const Products = () => {
         image={Nike270}
         title="Nike Air Max 270"
         subTitle="Men's Shoe"
-        details="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus, atque."
+        details="Lorem ipsum dolor sit amet consectetur"
         sizes={["36", "38", "40", "42", "44"]}
-        price={199.99}
+        price={199}
         /**/
         image1={Nike97}
         title1="Nike Air Max 97"
         subTitle1="We Men's Shoe"
-        details1="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus, atque."
+        details1="Lorem ipsum dolor sit amet consectetur."
         sizes1={["3", "38", "4", "42", "44"]}
-        price1={125.99}
+        price1={125}
         /**/
         image2={Nikesh}
         title2="Nike Air Force"
         subTitle2="We Men's Shoe"
-        details2="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus, atque."
+        details2="Lorem ipsum dolor sit amet consectetur."
         sizes2={["3", "38", "4", "42", "44"]}
-        price2={125.99}
+        price2={125}
       />
     </>
   );
